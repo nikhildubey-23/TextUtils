e4e03ddd1ad076caf5256ce9874a060a7146ff81
nikhildubey-23 <ravannick0089@gmail.com>
2025-01-21 16:45:33 +0530

    adding the path to the urls.py section

--- textutils/urls.py
+++ textutils/urls.py
@@ -20,4 +20,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('analyze/',views.analyze,name='analyze'),
+    path('contact/',views.contact,name='contact'),
+    path('about/',views.about,name='about'),
 ]
