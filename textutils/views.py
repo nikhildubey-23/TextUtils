478787585192280fa7fc54626ae1af89780a7560
Swarnav Das <164478477+swarnavdas15@users.noreply.github.com>
2025-01-23 13:55:47 +0530

    Update views.py
    This update can deal with more than one analysis prompt invoke

--- textutils/views.py
+++ textutils/views.py
@@ -26,12 +26,27 @@ def analyze(request):
 
     if charactercounter == 'on':
         analyzed = str(len(analyzed))
+#This condition invoke for more than one analysis prompt call
+    if (removepunc == 'on' and charactercounter == 'on'):
+        punctuation = '''.,;:!?'"-–—()[]{}\/|~`@#$%^&*_+=<>…'''
+        analyzed = ''.join(char for char in analyzed if char not in punctuation)
+        analyzedcout = str(len(analyzed))
+
+    if (uppercase != 'on' and charactercounter != 'on'):
+        analyzed = analyzed.upper()
+        analyzedcout = str(len(analyzed))
+     
+    if (removepunc != 'on' and uppercase != 'on'):
+         punctuation = '''.,;:!?'"-–—()[]{}\/|~`@#$%^&*_+=<>…'''
+         analyzed = ''.join(char for char in analyzed if char not in punctuation)
+         analyzed = analyzed.upper()
+        #By-Swarnav Das
 
     if (removepunc != 'on' and uppercase != 'on' and charactercounter != 'on'):
         back = '''<a href="/"><button style="width: 100px; height: 50px; background-color:#ff0000; color: white; border: none; border-radius: 4px; cursor: pointer;">Back</button></a>'''
         return HttpResponse('Please select any operation and try again \n' + back)
     
-    params = {'purpose': 'Analyzed Text', 'analyzed_text': analyzed}
+    params = {'purpose': 'Analyzed Text', 'analyzed_text': analyzed , 'analyzed_count': analyzedcout}
 
     return render(request, 'analyze.html', params)  
 def contact(request):
